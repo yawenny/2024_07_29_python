6c6e8515bba8748c5807de926c17bb6ca20e86e5
yawenny <yawen16@yahoo.com.tw>
2024-08-21 14:00:19 +0000

    modify0821

---
+++ lesson8/lesson8_2.py
@@ -0,0 +1,21 @@
+#自訂tools module (tools.py內有get_status_message的function)
+from widget import tools
+
+while True:
+    try:
+        name=str(input("請輸入姓名:"))
+        height=float(input('請輸入身高:'))
+        weight=float(input('請輸入體重:'))
+        bmi=round(weight/(((height*0.01))**2),ndigits=2)
+        message = tools.get_status_message(bmi)
+        print(f"{name} 的 bmi 為 {bmi},為{message}")
+    except ValueError:
+        print("格式錯誤")
+        continue
+    stuff = input("請問是否繼續輸入資料 ('q':離開,任意鍵:繼續)?")
+
+    if stuff == 'q':
+        break
+    
+
+print("應用程式結束")
