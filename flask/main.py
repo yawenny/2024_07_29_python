ce140036cfae2c119e740c784a6336978cf22ccb
yawenny <yawen16@yahoo.com.tw>
2024-10-09 13:20:47 +0000

    修改main的內容

--- flask/main.py
+++ flask/main.py
@@ -5,4 +5,6 @@ app = Flask(__name__)
 #呼叫route實體方法,根目錄,decorator要用@
 @app.route("/")
 def hello_world():
-    return "<h1>Hello, World!</h1>"#html字串
+    return '''<h1>這是python的project</h1>
+            <p>這是在codespace環境開發的</p>
+            '''
